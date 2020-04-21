fddd7c35dd97c46a6ac2b7bb354d3ff13f149ec1
Yueying <wangyy395@gmail.com>
2020-04-21 18:06:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+});
+   
+
+
+
+function updateDOM() {
+    let p = document.getElementById("text");
+    p.textContent = "This is really cool!";
+}
